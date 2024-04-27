dc0781bd0548746f5d807596d703eff0e6632b6f
sergheizagaiciuc <serghei.zagaiciuc@gmail.com>
2024-04-27 18:51:10 +0300

    commit it all

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 // Import necessary modules
 const crypto = require('crypto');
 
-function encryptSecret(secret, privateKey) {
+function mode1_encryptSecret(secret, privateKey) {
     const encryptedData = crypto.privateEncrypt({
         key: privateKey,
         padding: crypto.constants.RSA_PKCS1_PADDING
@@ -12,7 +12,7 @@ function encryptSecret(secret, privateKey) {
     return encryptedData.toString('base64');
 }
 
-function encryptContent(content, secret) {
+function mode1_encryptContent(content, secret) {
     const iv = crypto.randomBytes(8).toString('hex');
     const cipher = crypto.createCipheriv('aes-256-cbc', secret, iv);
 
@@ -21,6 +21,32 @@ function encryptContent(content, secret) {
 
     return iv + encryptedData;
 }
+
+function mode1_decryptSecret(sign, publicKey) {
+    const decryptedData = crypto.publicDecrypt({
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_PADDING
+    }, Buffer.from(sign, 'base64'));
+
+    return decryptedData.toString();
+}
+
+function mode1_decryptContent(encryptedContent, secret) {
+    const iv = encryptedContent.slice(0, 16);
+    const encryptedData = Buffer.from(encryptedContent.slice(16), 'base64');
+
+    const decipher = crypto.createDecipheriv('aes-256-cbc', secret, iv);
+    decipher.setAutoPadding(false);
+    let decryptedData = decipher.update(encryptedData);
+    decryptedData = Buffer.concat([decryptedData, decipher.final()]);
+
+    return decryptedData.toString();
+}
+
+
+
+
+
 
 function randomString(length) {
 	const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -32,6 +58,13 @@ function randomString(length) {
     return result;
 }
 
+
+
+
+
+
+
+// start app code - MODE1 - encrypt 
 function clearResults()
 {
 	document.getElementById("x-sign").value = "";
@@ -44,20 +77,26 @@ function setResults(sign, encryptedContent)
 	document.getElementById("body").value = encryptedContent;
 }
 
-function useExampleKey(e)
+function mode1_encrypt_useExampleKey(e)
 {
 	try{
 		e.preventDefault();
 		
-		const exampleKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
-                                             "MIIBOgIBAAJBAKj34GkxFhD90vcNLYLInFEX6Ppy1tPf9Cnzj4p4WGeKLs1Pt8Qu\n" +
-                                             "KUpRKfFLfRYC9AIKjbJTWit+CqvjWYzvQwECAwEAAQJAIJLixBy2qpFoS4DSmoEm\n" +
-                                             "o3qGy0t6z09AIJtH+5OeRV1be+N4cDYJKffGzDa88vQENZiRm0GRq6a+HPGQMd2k\n" +
-                                             "TQIhAKMSvzIBnni7ot/OSie2TmJLY4SwTQAevXysE2RbFDYdAiEBCUEaRQnMnbp7\n" +
-                                             "9mxDXDf6AU0cN/RPBjb9qSHDcWZHGzUCIG2Es59z8ugGrDY+pxLQnwfotadxd+Uy\n" +
-                                             "v/Ow5T0q5gIJAiEAyS4RaI9YG8EWx/2w0T67ZUVAw8eOMB6BIUg0Xcu+3okCIBOs\n" +
-                                             "/5OiPgoTdSy7bcF9IGpSE8ZgGKzgYQVZeN97YE00\n" +
-                                             "-----END RSA PRIVATE KEY-----";
+		const exampleKey = "-----BEGIN RSA PRIVATE KEY-----\n" + 
+			"MIICXQIBAAKBgQClQPjXnFwCbuJq17LXTav9a04Xokg6JopiA0gQcYzMC9sqImeG\n" + 
+			"s3o7wtT7N8TcgPCMdEtYJmMTaTSGUSBeW6gdSdWmyTgreyJ6l08Ms6iP9hawPS+K\n" + 
+			"beklQ1Pax+zJVg/tT7Bmvd0rZZwdIRrViVm2/5HpcvBX0aCXkJAPRIVXPwIDAQAB\n" + 
+			"AoGAOyHrqWN2/RvmgyraAPb3M0Bhek2EoOJHpFjeQZwQMLeRXhtfhjDU7WuDQL2t\n" + 
+			"AOZWrTuz9kAONdTwiZugBIOxvO3JIxEyx8i66kEgPhPuVOBYie9d9KevegsNLxbp\n" + 
+			"wuPeSRTTIBuYPAJ2aHGwLhHh5OuvC+6pZayyIGS6isDCGqECQQDaKoQFf59JcZ4o\n" + 
+			"y0k+bUL7jQOj/1tNy0VtsYjzfaf1wl4Kro28jTroXZiCUEbTao/z2h9bHqoo0HmR\n" + 
+			"ZPjeR9fPAkEAwelqVCKk5G2SmpFaKBpbb3r9FfxGxjU9BZkgY3OCjs4I+Z3Piy6x\n" + 
+			"Q87/Ub9e7dORqORuQbkzPfzJOMOKVsX1kQJAX27RqYYWK45j3Pxv4brx3g/lU8vU\n" + 
+			"KMeOa1mJytlgq4SGlq2cmqo85oBqwjZThQ/MQKNdrAJR9OCdDRjaNIHAyQJBAK4L\n" + 
+			"GJpndeRozHrbFzaDYaoPk3TWN5fTVO/fXoiktnwCRV/12sArqoMYGWWABG4lxMj4\n" + 
+			"LlXjKjDq4JiIOXRkvWECQQC01Zv06ZLOpdRWBBn5MH6HUrLliAnmTgwb5tReRqkM\n" + 
+			"De6l6ciHzyGSEfm+qMLigh6MTodGuDE5Qr2PSpvEojZW\n" + 
+			"-----END RSA PRIVATE KEY-----";
 		
 		document.getElementById("key").value = exampleKey;
 	}
@@ -96,7 +135,7 @@ function copyBodyToClipboard(e)
 	navigator.clipboard.writeText(str); 
 }
 
-function startEncrypt(e)
+function mode1_startEncrypt(e)
 {
 	try{
 		e.preventDefault();
@@ -107,8 +146,8 @@ function startEncrypt(e)
 		const request = document.getElementById("request").value;
 
 		const secret = randomString(32) // 'RandomString32CharactersLength12'; // 32 characters; must be randomly generated in every request
-		const sign = encryptSecret(secret, key); // add to the request header as 'x-sign'
-		const encryptedContent = encryptContent(request, secret); // set this to be the request body
+		const sign = mode1_encryptSecret(secret, key); // add to the request header as 'x-sign'
+		const encryptedContent = mode1_encryptContent(request, secret); // set this to be the request body
 
 		setResults(sign, encryptedContent);
 	}
@@ -116,11 +155,79 @@ function startEncrypt(e)
 		console.error(e);
 	}
 }
+// end mode1_encrypt 
 
-document.getElementById("go").addEventListener("click", startEncrypt);
 
-document.getElementById("useExampleKey").addEventListener("click", useExampleKey);
-document.getElementById("useExampleRequest").addEventListener("click", useExampleRequest);
+// mode1_decrypt
+function mode1_decrypt_clearResults()
+{
+	document.getElementById("body").value = "";
+}
 
-document.getElementById("copyxsign").addEventListener("click", copyXSignToClipboard);
-document.getElementById("copybody").addEventListener("click", copyBodyToClipboard);
+function mode1_decrypt_setResults(results)
+{
+	document.getElementById("body").value = results;
+}
+
+function mode1_decrypt_useExampleKey(e)
+{
+	try{
+		e.preventDefault();
+		
+		const exampleKey = "-----BEGIN PUBLIC KEY-----\n" + 
+			"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQClQPjXnFwCbuJq17LXTav9a04X\n" + 
+			"okg6JopiA0gQcYzMC9sqImeGs3o7wtT7N8TcgPCMdEtYJmMTaTSGUSBeW6gdSdWm\n" + 
+			"yTgreyJ6l08Ms6iP9hawPS+KbeklQ1Pax+zJVg/tT7Bmvd0rZZwdIRrViVm2/5Hp\n" + 
+			"cvBX0aCXkJAPRIVXPwIDAQAB\n" + 
+			"-----END PUBLIC KEY-----";
+		
+		document.getElementById("key").value = exampleKey;
+	}
+	catch(e){
+		console.error(e);
+	}
+}
+
+function mode1_startDecrypt(e)
+{
+	try{
+		e.preventDefault();
+
+		mode1_decrypt_clearResults();
+
+		const xsign = document.getElementById("x-sign").value;
+		const key = document.getElementById("key").value;
+		const encryptedContent = document.getElementById("enccontent").value;
+
+		decryptedSecret = mode1_decryptSecret(xsign, key);
+		console.log("decrypted secret", decryptedSecret);
+
+		decryptedContent = mode1_decryptContent(encryptedContent, decryptedSecret);
+
+		mode1_decrypt_setResults(decryptedContent);
+	}
+	catch(e){
+		alert(e);
+	}
+}
+// end mode1_decrypt 
+
+// bad code, refactor at some point 
+if(document.getElementById("encrypt-mode1"))
+{
+	document.getElementById("encrypt-mode1").addEventListener("click", mode1_startEncrypt);
+
+	document.getElementById("useExampleKey").addEventListener("click", mode1_encrypt_useExampleKey);
+	document.getElementById("useExampleRequest").addEventListener("click", useExampleRequest);
+
+	document.getElementById("copyxsign").addEventListener("click", copyXSignToClipboard);
+	document.getElementById("copybody").addEventListener("click", copyBodyToClipboard);
+}
+else if(document.getElementById("decrypt-mode1")){
+	document.getElementById("decrypt-mode1").addEventListener("click", mode1_startDecrypt);
+
+	document.getElementById("useExampleKey").addEventListener("click", mode1_decrypt_useExampleKey);
+}
+else{
+	alert("unknown page, this is an error in program, please talk to tech")
+}
